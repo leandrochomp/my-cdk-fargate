a2d1f39c71c663776a15b860b249ba47e1da9d95
leandro.nascimento@dnx.solutions <leandro.nascimento@dnx.solutions>
2021-10-18 13:27:08 +1100

    Adding fargate configs

--- fargate-app/lib/fargate-app-stack.ts
+++ fargate-app/lib/fargate-app-stack.ts
@@ -1,9 +1,30 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 
 export class FargateAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "VPCByCdk", {
+      maxAzs: 3 // Default is all AZs in region
+    });
+
+    const cluster = new ecs.Cluster(this, "ClusterByCdk", {
+      vpc: vpc // Points to VCP created by VPCByCdk
+    });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "FargateServiceByCdk", {
+      cluster: cluster, // Required
+      cpu: 512, // Default is 256
+      desiredCount: 2,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")
+      },
+      memoryLimitMiB: 2048, // Default is 512
+      publicLoadBalancer: true // Default is false
+    });
   }
 }
